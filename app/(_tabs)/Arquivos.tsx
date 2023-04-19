2593d0c1c45726a53a664efb6ec880682a09790d
Daniel <bastosydaniel@gmail.com>
2023-04-19 15:14:56 -0300

    WIP: some changes on Arquivo template

--- app/(_tabs)/Arquivos.tsx
+++ app/(_tabs)/Arquivos.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import RecursoService from '../core/services/RecursoService'
+import { Recurso } from '../../models/Recurso'
+import * as nativeBase from "native-base";
 
-const Arquivos = () => {
+
+export default function listaRecursos() {
+
+  const { listaRecursos } = RecursoService()
+
+  type RenderRecursoProps = {
+    item: Recurso
+  }
+
+  const RenderRecurso = ({ item }: RenderRecursoProps) => {
+    return (<Text>
+      {item.nomeArquivo}
+    </Text>)
+  }
+
   return (
     <View>
+      <FlatList
+        ListHeaderComponent={() => (
+          <nativeBase.Heading fontFamily={'Poppins'} fontSize="20" p="2" marginLeft="4">
+            Meus Recursos
+          </nativeBase.Heading>
+        )}
+        data={listaRecursos}
+        renderItem={RenderRecurso}
+        keyExtractor={(item) => item.id.toString()}
+      />
+
       <Text>Arquivos</Text>
     </View>
   )
 }
-
-export default Arquivos
