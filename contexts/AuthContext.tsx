f2ed0787e01f8aa6c4aecf39c2ba4e554cbef321
Felipe Beser <felipebeserx@gmail.com>
2023-06-19 12:42:27 -0300

    feat: AuthContext

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 import { createContext, useContext, useState } from 'react'
+import { API } from '../http/API';
 
 interface AuthProps {
   authState?: {token: string | null, authenticated: boolean | null},
-  // onLogin?: (cpf: string, senha: string) => Promise<any>,
-  // onLogout?: () => Promise<any>,
+  onLogin?: (cpf: string, senha: string) => Promise<any>,
+  onLogout?: () => Promise<any>,
 }
 
 export const AuthContext = createContext<AuthProps>({})
@@ -11,15 +12,6 @@ export const AuthContext = createContext<AuthProps>({})
 export function useAuth() {
   return useContext(AuthContext);
 }
-
-const login = () => {
-
-}
-
-const logout = () => {
-
-}
-
 
 export const AuthProvider = ({children}) => {
   const [authState, setAuthState] = useState<{
@@ -30,12 +22,21 @@ export const AuthProvider = ({children}) => {
     authenticated: null
   })
 
+  const login = async (cpf: string, senha: string) => {
+    const { data } = await API.post('Usuario/LogarUsuario', {cpf, senha});
+    setAuthState({ token: data.token, authenticated: true})
+  }
+
+  const logout = async () => {
+    setAuthState({token: null, authenticated: false})
+  }
+
   return (
     <AuthContext.Provider
     value={{
       authState,
-      // onLogin: login,
-      // onLogout: logout,
+      onLogin: login,
+      onLogout: logout,
       // user,
     }}>
       {children}
