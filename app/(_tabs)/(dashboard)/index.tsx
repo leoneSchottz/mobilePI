3e33bdd9049fb6df1f1ac2de3bdcb3cd573486a8
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-06-24 12:22:35 -0300

    reactor: addition of atividades in dashboard

--- app/(_tabs)/(dashboard)/index.tsx
+++ app/(_tabs)/(dashboard)/index.tsx
@@ -1,46 +1,62 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { FlashList } from '@shopify/flash-list';
-import { getGruposByEstudanteIdByPeriodoIdWithFrequency } from '../../../core/services/GrupoService';
-import ListaGrupo from '../../../components/ListaGrupo';
-import ProfileScreen from '../../../components/Dashboard/ProfileScreen';
-import Colors from '../../../common/constants/Colors';
-import { useAuth } from '../../../contexts/AuthContext';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import { getGruposByEstudanteIdByPeriodoIdWithFrequency } from "../../../core/services/GrupoService";
+import ListaGrupo from "../../../components/ListaGrupo";
+import ProfileScreen from "../../../components/Dashboard/ProfileScreen";
+import Colors from "../../../common/constants/Colors";
+import { useAuth } from "../../../contexts/AuthContext";
+import { getAllAtividades } from "../../../core/services/ativade/AtividadeService";
+import ProximasAtividades from "./ucs/ProximasAtividades";
 
 export default function ListaUC() {
-
   const idPeriodo = 2;
   const idEstudante = useAuth().authState.userData.estudanteId;
-  const {grupos, isLoaded} = getGruposByEstudanteIdByPeriodoIdWithFrequency(idEstudante, idPeriodo)
+  const { grupos, isLoaded } = getGruposByEstudanteIdByPeriodoIdWithFrequency(
+    idEstudante,
+    idPeriodo
+  );
 
   return (
     <>
-    {isLoaded
-    ? <View style={styles.container}>
-        <FlashList
-          ListHeaderComponent={<HeaderCursos/>}
-          data={grupos}
-          estimatedItemSize={8}
-          numColumns={1}
-          renderItem = { ({item}) => <ListaGrupo {...item}/>}
-          keyExtractor={(item) => item.id.toString()}
+      {isLoaded ? (
+        <View style={styles.container}>
+          <ProximasAtividades/>
+          <FlashList
+            ListHeaderComponent={<HeaderCursos />}
+            data={grupos}
+            estimatedItemSize={8}
+            numColumns={1}
+            renderItem={({ item }) => <ListaGrupo {...item} />}
+            keyExtractor={(item) => item.id.toString()}
           />
-        {/* <ProfileScreen /> */}
-      </View>
-    :<Text>Carregando..</Text>}
+          {/* <ProfileScreen /> */}
+        </View>
+      ) : (
+        <Text>Carregando..</Text>
+      )}
     </>
-  )
+  );
 }
 
-  const HeaderCursos = () => {
-    return (
-      <Text style={{paddingHorizontal: 30, marginTop: 20, fontSize: 28, fontWeight: '600', letterSpacing: -0.5}}>Meus Cursos</Text>
-    )
-  }
+const HeaderCursos = () => {
+  return (
+    <Text
+      style={{
+        paddingHorizontal: 30,
+        marginTop: 20,
+        fontSize: 28,
+        fontWeight: "600",
+        letterSpacing: -0.5,
+      }}
+    >
+      Meus Cursos
+    </Text>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.light.background
+    backgroundColor: Colors.light.background,
   },
-
-})
+});
