5a3fe6596617da724d93b9a4ab7faaa67231f2a6
Felipe Beser <felipebeserx@gmail.com>
2023-07-07 11:30:11 -0300

    removed unused and outdated files and packages, updated app name

--- app/(_tabs)/(menu)/forum/_layout.tsx
+++ app/(_tabs)/(menu)/forum/_layout.tsx
@@ -5,9 +5,6 @@ const forumLayout = () => {
         <Stack initialRouteName='Forum' screenOptions={{headerShown: true}}>
                 <Stack.Screen name='Comunidades' options={{ headerTitle: 'Fórum'}}/>
                 <Stack.Screen name='[chapterAssuntoId]' options={{ headerTitle: 'Tópico'}}/>
-                <Stack.Screen name='Forum' />
-                <Stack.Screen name='Topico' />
-                <Stack.Screen name='Perguntar'/>
                 <Stack.Screen name='NovaPergunta'options={{ headerTitle: 'Nova Pergunta'}}/>
         </Stack>
     )
